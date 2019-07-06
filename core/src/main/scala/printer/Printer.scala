1545ec742fef5da1df681e660ff0822fcb8eae80
Karnaj <yoan.geran@hotmail.fr>
2019-07-06 02:12:37 +0200

    Improve pretty printer.

--- core/src/main/scala/printer/Printer.scala
+++ core/src/main/scala/printer/Printer.scala
@@ -7,49 +7,63 @@ import stainless.lang._
 import _root_.trees._
 
 object Printer {
-  def pprint(e: Tree, ind: Int): String = {
+  def pprint(e: Tree): String = {
     e match {
       case Var(id, x) => x
-      case UnitLiteral => ""
+      case UnitLiteral => "()"
       case BoolLiteral(b) => b.toString
       case NatLiteral(n) => n.toString
-      case IfThenElse(c, e1, e2) => {
-        val cs = pprint(c, 0)
-        val e1s = pprint(e1, ind)
-        val e2s = pprint(e2, ind)
-        s"""${" " * ind}if(${cs}) {
+      case IfThenElse(c, e1, e2) =>
+        val cs = pprint(c)
+        val e1s = pprint(e1).replaceAll("\n", "\n  ")
+        val e2s = pprint(e2).replaceAll("\n", "\n  ")
+        s"""if(${cs}) {
         |  ${e1s}
-        |{" " * ind}} else {
+        |else {
         |  ${e2s}
-        |${" " * ind}}""".stripMargin
-      }
-      case Lambda(tp, body) => s"${" " * ind} fun ${pprint(body, ind + 2)}"
-      case App(e1, e2) => s"(${pprint(e1, ind)})(${pprint(e2, ind)})"
-      case Fix(body) => s"Fix(${pprint(body, ind)})"
-      case LeftTree(b) => s"Left(${pprint(b, ind)})"
-      case RightTree(b) => s"Rigt(${pprint(b, ind)})"
+        |}""".stripMargin
+      case Lambda(tp, Bind(Some(x), body)) =>
+        s"""fun ${pprint(x)} => {
+        |  ${pprint(body).replaceAll("\n", "\n  ")}
+        |}""".stripMargin
+      case Lambda(tp, Bind(None(), body)) =>
+        s"""fun () => {
+        |  ${pprint(body).replaceAll("\n", "\n  ")}
+        |}""".stripMargin
+      case App(e1, e2) => s"(${pprint(e1)})(${pprint(e2)})"
+      case Fix(body) =>
+      s"""Fix(
+       |  ${pprint(body).replaceAll("\n", "\n  ")}
+       |)""".stripMargin
+      case LeftTree(b) => s"Left(${pprint(b)})"
+      case RightTree(b) => s"Rigt(${pprint(b)})"
+
       case EitherMatch(t, t1, t2) =>
-        val ts = pprint(t, ind)
-        val t1s = pprint(t1, ind)
-        val t2s = pprint(t2, ind)
+        val ts = pprint(t)
+        val t1s = pprint(t1).replaceAll("\n", "\n    ")
+        val t2s = pprint(t2).replaceAll("\n", "\n    ")
         s"""${ts} match {
-        |  case Left() => ${t1s}
-        |  case Right() => ${t2s}
+        |  case Left() => {
+        |    ${t1s}
+        |  }
+        |  case Right() => {
+        |    ${t2s}
+        |  }
         |}""".stripMargin
       case Bind(Some(Var(_, x)), b) =>
-        s"""${x} : {
-        |  ${pprint(b, ind)}
-        |}""".stripMargin
-      case Bind(None(), b) =>
-        s"${pprint(b, ind)}"
+        s"""${x} => {
+        |  ${pprint(b).replaceAll("\n", "\n  ")}
+        |} """.stripMargin
+      case Bind(None(), b) => pprint(b)
       case LetIn(tp, v, Bind(Some(x), b)) =>
-          val tv = pprint(v, ind)
-          val tx = pprint(x, ind)
-          val tb = pprint(b, ind)
-          s"""Let ${tx} = ${tv} in ${tb}
+          val tv = pprint(v)
+          val tx = pprint(x)
+          val tb = pprint(b)
+          s"""val ${tx} = ${tv}
+          |${tb}
           """.stripMargin
-      case Primitive(op, arg::Nil())  => s"op(${pprint(arg, 0)})"
-      case Primitive(op, arg1::arg2::Nil())  => s"op(${pprint(arg1, 0)}, ${pprint(arg2, 0)})"
+      case Primitive(op, arg::Nil())  => s"op(${pprint(arg)})"
+      case Primitive(op, arg1::arg2::Nil())  => s"op(${pprint(arg1)}, ${pprint(arg2)})"
       case _ => "e"
     }
   }
