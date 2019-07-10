08726f37cd147959b2a699eeb578bb7471fb1fa1
Jad Hamza <jad.hamza@epfl.ch>
2019-07-10 16:20:06 +0200

    Add missing files

---
+++ core/src/test/scala/interpreter/EvalFileSuite.scala
@@ -0,0 +1,10 @@
+import Utils._
+import org.scalatest.FunSuite
+
+class EvalFileSuite extends FunSuite {
+  for (f <- files("examples", _.endsWith("sc"))) {
+    test(s"Running file $f") {
+      Main.evalFile(f)
+    }
+  }
+}
