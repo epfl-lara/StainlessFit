1705a48c350b62553e67756890e85818e9c85e77
Karnaj <yoan.geran@hotmail.fr>
2019-07-12 11:27:25 +0200

    Tests.

--- core/src/test/scala/parser/ParserSuite.scala
+++ core/src/test/scala/parser/ParserSuite.scala
@@ -11,20 +11,4 @@ class ParserSuite extends FunSuite {
   test("parser is LL1") {
     assert(expression.isLL1)
   }
-
-  test("parse arithmetic operations") {
-    val s = "(2 + 3) * 4 + 2 * 3 / 2 - 1".toIterator
-    apply(ScalaLexer.apply(s)) match {
-      case Parsed(e, _) => assert(evaluate(e, 1000) == NatLiteral((2 + 3) * 4 + 2 * 3 / 2 - 1))
-      case _ => assert(false)
-    }
-  }
-
-  test("parse function definition") {
-    val s = "val f = fun (x: Nat) => { x } in f(2)".toIterator
-    apply(ScalaLexer.apply(s)) match {
-      case Parsed(e, _) => assert(true || evaluate(e, 1000) == NatLiteral(2))
-      case _ => assert(false)
-    }
-  }
 }
