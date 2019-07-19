94f68ce3523a73f0768938892ccca6bb85ca59d6
Karnaj <yoan.geran@hotmail.fr>
2019-07-19 09:58:34 +0200

    ...

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -10,39 +10,42 @@ import java.io.File
 
 object Main {
 
-  def evalFile(f: File): Tree = {
+  val assertFun = """def assert(b: Bool): Unit = { if(b) () else val x = Error("Assertion failed") in () }"""
+
+  def evalFile(f: File): (Result, Tree) = {
     val s = scala.io.Source.fromFile(f).getLines.mkString("\n")
-    val assertFun = """def assert(b: Bool): Unit = { if(b) () else val x = Error("Assertion failed") in () }"""
     val it = (assertFun + s).toIterator
     ScalaParser.apply(ScalaLexer.apply(it)) match {
       case ScalaParser.Parsed(value, _) =>
-        println(TypeChecker.newInfer(value))
+        val ty = TypeChecker.infer(value)
         Interpreter.evaluate(value, 1000000000) match {
           case ErrorTree(error, _) => throw new Exception(s"Error during evaluation.\n${error}")
-          case v => v
+          case v => (ty, v)
         }
       case t =>
         throw new Exception("Error during parsing:\n" + t)
     }
   }
 
-  /*def typeCheckFile(f: File): Tree = {
+  def typeCheckFile(f: File): Result = {
     val s = scala.io.Source.fromFile(f).getLines.mkString("\n")
     val it = (assertFun + s).toIterator
     ScalaParser.apply(ScalaLexer.apply(it)) match {
       case ScalaParser.Parsed(value, _) =>
-          TypeChecker.infer()
-        }
+        TypeChecker.infer(value)
       case t =>
         throw new Exception("Error during parsing:\n" + t)
     }
-  }*/
+  }
 
-  def evalFile(s: String): Tree = evalFile(new File(s))
+  def evalFile(s: String): (Result, Tree) = evalFile(new File(s))
+
+  def typeCheckFile(s: String): Result = typeCheckFile(new File(s))
 
   def printHelp() = {
     println(
       """|Usage: run eval FILE
+      |
       """.stripMargin)
   }
 
@@ -51,7 +54,11 @@ object Main {
     else {
       args(0) match {
         case "eval" if (args.length == 2) =>
-          println(Printer.pprint(evalFile(args(1))))
+          val (ty, v) = evalFile(args(1))
+          println(ty)
+          println(Printer.pprint(v))
+        case "typecheck" if (args.length == 2) =>
+          println(typeCheckFile(args(1)))
         case _ =>
           printHelp()
       }
