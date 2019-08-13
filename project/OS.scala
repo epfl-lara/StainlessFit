400f988698891883cd4088d654a4ac865a04e092
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-08-13 16:04:05 +0200

    Fix ScalaZ3 JAR lookup

---
+++ project/OS.scala
@@ -0,0 +1,11 @@
+
+object OS {
+  val osInf = Option(System.getProperty("os.name")).getOrElse("")
+
+  val isUnix    = osInf.indexOf("nix") >= 0 || osInf.indexOf("nux") >= 0
+  val isWindows = osInf.indexOf("Win") >= 0
+  val isMac     = osInf.indexOf("Mac") >= 0
+
+  val name = if (isWindows) "win" else if (isMac) "mac" else "unix"
+  val arch = System.getProperty("sun.arch.data.model")
+}
