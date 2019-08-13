38ac3b65ae7ff48d646b8ed4417407ea60151d78
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-08-13 17:31:38 +0200

    Improve CLI

---
+++ core/src/main/scala/Config.scala
@@ -0,0 +1,61 @@
+import scopt.OParser
+import java.io.File
+
+sealed abstract class Mode
+case object Eval      extends Mode
+case object TypeCheck extends Mode
+
+case class Config(
+  mode: Mode       = null,
+  file: File       = null,
+  watch: Boolean   = false,
+  debug: Boolean   = false,
+  verbose: Boolean = false,
+)
+
+object Config {
+
+  def parse(args: Array[String]): Option[Config] =
+    OParser.parse(options, args, Config())
+
+  private val builder = OParser.builder[Config]
+  private val options = {
+    import builder._
+
+    OParser.sequence(
+      programName("stainless-core"),
+      head("StainlessCore", "0.1"),
+      help("help").text("Prints help information"),
+      opt[Unit]("verbose")
+        .action((_, c) => c.copy(verbose = true))
+        .text("Enable verbose output"),
+      opt[Unit]("debug")
+        .action((_, c) => c.copy(debug = true))
+        .text("Enable debug output"),
+      opt[Unit]("watch")
+        .action((_, c) => c.copy(watch = true))
+        .text("Re-run on file modification"),
+      cmd("eval")
+        .action((_, c) => c.copy(mode = Eval))
+        .text("Evaluate the given file")
+        .children(
+          arg[File]("<file>...")
+            .required()
+            .action((f, c) => c.copy(file = f)),
+        ),
+      cmd("typecheck")
+        .action((_, c) => c.copy(mode = TypeCheck))
+        .text("Type check the given file")
+        .children(
+          arg[File]("<file>...")
+            .required()
+            .action((f, c) => c.copy(file = f)),
+        ),
+      checkConfig {
+        case c if c.mode == null => failure("Please specify a command: eval, typecheck")
+        case c if !c.file.exists => failure(s"File not found: ${c.file}")
+        case _ => success
+      }
+    )
+  }
+}
