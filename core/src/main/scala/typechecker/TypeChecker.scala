ead611d6164e0b2b7fffe042c98591ecfbf36901
Jad Hamza <jad.hamza@epfl.ch>
2020-04-09 09:44:12 +0200

    Backport more changes from scaladep

--- core/src/main/scala/typechecker/TypeChecker.scala
+++ core/src/main/scala/typechecker/TypeChecker.scala
@@ -8,98 +8,13 @@ import parser.FitParser
 
 import Derivation._
 
-
-object Context {
-  def empty(implicit rc: RunContext): Context = Context(Map(), Set(), 0, 0)
-  def empty(max: Int)(implicit rc: RunContext): Context = Context(Map(), Set(), 0, max)
-}
-
-case class Context(
-  val termVariables: Map[Identifier, Tree],
-  val typeVariables: Set[Identifier],
-  val level: Int,
-  val n: Int // All variables in the context must have an identifier strictly smaller than n.
-)(implicit rc: RunContext) extends Positioned {
-
-  def bind(i: Identifier, t: Tree): Context = {
-    if (termVariables.contains(i)) throw new Exception("Already bound " + i.toString)
-    copy(
-      termVariables = termVariables.updated(i, t)
-    )
-  }
-
-  def addTypeVariable(i: Identifier): Context = copy(typeVariables = typeVariables + i)
-
-  def bindFresh(s: String, t: Tree): (Identifier, Context) = (Identifier(n, s), bind(Identifier(n, s), t).copy(n = n+1))
-
-  def getFresh(s: String): (Identifier, Context) = (Identifier(n, s), copy(n = n+1))
-
-  def contains(id: Identifier): Boolean = termVariables.contains(id)
-
-  def getTypeOf(id: Identifier): Option[Tree] = termVariables.get(id)
-
-  def addEquality(t1: Tree, t2: Tree): Context = bindFresh("eq", EqualityType(t1, t2))._2
-
-  def incrementLevel: Context = copy(level = level + 1)
-
-  def containsVarOfType(tp: Tree): Boolean =
-    termVariables.exists(_._2 == tp)
-
-  def getVarOfType(tp: Tree): Option[Identifier] =
-    termVariables.find(_._2 == tp).map(_._1)
-
-  override def toString: String = {
-    Printer.asString(this)(rc)
-  }
-
-  def hasRefinement: Boolean = {
-    termVariables.exists { case (v, tp) =>
-      tp match {
-        case RefinementType(_, _) => true
-        case _ => false
-      }
-    }
-  }
-
-  def replace(id: Identifier, t: Tree)(implicit rc: RunContext): Context = {
-    copy(
-      termVariables = termVariables.map {
-        case (v, tp) => (v, tp.replace(id, t))
-      }
-    )
-  }
-}
-
-case class Tactic[A,B](apply: (A, (A => Option[B])) => Option[B]) {
-  def ||(other: Tactic[A,B]): Tactic[A,B] = this.or(other)
-  def or(other: Tactic[A,B]): Tactic[A,B] =
-    Tactic {
-      case (g, subgoalSolver) =>
-        apply(g, subgoalSolver) orElse
-        other.apply(g, subgoalSolver)
-    }
-
-  def andThen(other: Tactic[A,B]): Tactic[A,B] =
-    Tactic {
-      case (g, subgoalSolver) =>
-        apply(g, sg => other.apply(sg, subgoalSolver))
-    }
-
-  def repeat: Tactic[A,B] = {
-    def repeatApply(goal: A, subgoalSolver: A => Option[B]): Option[B] = {
-      apply(goal, sg => repeatApply(sg, subgoalSolver))
-    }
-    Tactic[A,B](repeatApply)
-  }
-}
-
 class TypeChecker(implicit val rc: RunContext)
-  extends TypeCheckerProvenRules
-  with TypeCheckerUnprovenRules
-  with TypeCheckerUnsoundRules
-  with TypeCheckerSMTRules
-  with TypeCheckerControlRules
-  with TypeCheckerMetaRules {
+  extends ProvenRules
+  with UnprovenRules
+  with UnsoundRules
+  with SMTRules
+  with ControlRules
+  with MetaRules {
 
   val deterministicTypeChecking: Tactic[Goal, (Boolean, NodeTree[Judgment])] =
     CheckBool.t || CheckNat.t || CheckUnit.t || CheckVar.t ||
@@ -152,24 +67,24 @@ class TypeChecker(implicit val rc: RunContext)
 }
 
 object TypeChecker {
-  def debugs(rc: RunContext, g: Goal, ruleName: String) = {
-    ruleNameDebug(rc, s"${"   " * g.c.level}$ruleName")
-    typeCheckDebug(rc, s"${"   " * g.c.level}Current goal ${g} $ruleName : ${g.c.toString.replaceAll("\n", s"\n${"   " * g.c.level}")}\n")
+  def debugs(g: Goal, ruleName: String)(implicit rc: RunContext) = {
+    ruleNameDebug(s"${"   " * g.c.level}$ruleName")
+    typeCheckDebug(s"${"   " * g.c.level}Current goal ${g} $ruleName : ${g.c.toString.replaceAll("\n", s"\n${"   " * g.c.level}")}\n")
   }
 
-  def ruleNameDebug(rc: RunContext, s: => String): Unit = {
+  def ruleNameDebug(s: => String)(implicit rc: RunContext): Unit = {
     if (rc.debugEnabled(DebugSection.Rule)) {
       rc.reporter.debug(s)
     }
   }
 
-  def typeCheckDebug(rc: RunContext, s: => String): Unit = {
+  def typeCheckDebug(s: => String)(implicit rc: RunContext): Unit = {
     if (rc.debugEnabled(DebugSection.TypeCheck)) {
       rc.reporter.debug(s)
     }
   }
 
-  def equalityDebug(rc: RunContext, s: => String): Unit = {
+  def equalityDebug(s: => String)(implicit rc: RunContext): Unit = {
     if (rc.debugEnabled(DebugSection.Equality)) {
       rc.reporter.debug(s)
     }
